e6e09843278bef9ae32671a4139f9cafd5ee90a0
ahnufmir <bsse24004@itu.edu.pk>
2025-11-02 03:01:53 +0500

    practising closures and hoisting

---
+++ script.js
@@ -0,0 +1,30 @@
+// function a() {
+//     var b = 10;
+//     c();
+//     function c(){
+//         console.log(b)
+//     }
+// }
+
+
+// a();
+
+// let d = 10
+// console.log(d)
+// function x() {
+//     var d = 20;
+//     console.log(d);
+// }
+// x();  
+
+// function x(){
+//     var x = 7;
+//     function y(){
+//         console.log(x);
+//     }
+//     return y;
+// }
+
+// var z = x();
+// console.log(z);
+// z();
