1e97b775d400f36e1b7c9a43faa9371a1d3b4c9f
Ahnuf Mir <mirahnaf55@gmail.com>
2025-12-03 02:50:15 -0500

    Promises

---
+++ Playing wtih Api / Promises/script.js
@@ -0,0 +1,76 @@
+// const pic = document.querySelector("#pic");
+// const nameUser= document.querySelector("#name");
+// const followers = document.querySelector("#followers");
+
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/users/ahnufmir');
+// xhr.onreadystatechange = function () {
+//     const state = (xhr.readyState);
+//     console.log(state);
+//     if(state === 4){
+//         const data = JSON.parse(this.responseText);
+//         console.log(data.avatar_url);
+//         console.log(data);
+//         pic.src = data.avatar_url;
+//         nameUser.innerHTML = data.login;
+//         followers.innerHTML = data.followers;
+
+
+//     }
+// }
+// xhr.send();
+
+
+// Fetch
+// const GITHUB_URL = "http://api.github.com/users/ahnufmir";
+// const user = fetch(GITHUB_URL);
+// console.log(user);
+
+// user.then(function(data){
+//     console.log(data);
+// })
+
+const cart = ["elon musk", "sam altman", "larry page", "steve jobs"];
+
+createOrder(cart)
+    .then(function(orderId){
+        console.log(orderId);
+    })
+    .then(function(orderId){
+        proceedToPayment(orderId);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+
+function createOrder(cart){
+    const promise = new Promise(function(resolve, reject){
+
+    if(!validateCart(cart)){
+        const error = "Cart is not validated";
+        reject(error);
+    }
+    else
+    {
+        const orderId = "233";
+        if(orderId){
+            resolve(orderId);
+        }
+    }
+
+    }
+)
+return promise;
+}
+
+function proceedToPayment(orderId){
+    return new Promise(
+        function(resolve, reject){
+            resolve("Payment Successful");
+        }
+    )
+}
+
+function validateCart(cart){
+    return false;
+}
