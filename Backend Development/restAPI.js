ee65786e1c8c76070c166adaa7bb34a9d0cad90c
Ahnuf Mir <mirahnaf55@gmail.com>
2026-01-23 01:57:35 +0500

    pushed new user through user and added into file

--- Backend Development/restAPI.js
+++ Backend Development/restAPI.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.urlencoded({ extended: false }));
 // for getting specfic user from any client other than browers
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => id === user.id);
-  return res.json(user);
-});
+// app.get("/api/users/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const user = users.find((user) => id === user.id);
+//   return res.json(user);
+// });
 
 // for listing all users from any client other than browser
 app.get("/api/users", (req, res) => {
@@ -26,19 +28,26 @@ app.get("/users", (req, res) => {
   res.send(html);
 });
 
-app 
-    .route("/api/users/:id")
-    .get((req, res) => {
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => id === user.id);
     return res.json(user);
-    })
-    .patch((req,res)=>{
-        return res.json({status:"pending"});
-    })
-    .delete((req,res)=>{
-        return res.json({status:"pending"});
-    })
+  })
+  .post((req, res) => {
+    const body = req.body;
+    users.push(body);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.send(body);
+    });
+  })
+  .patch((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "pending" });
+  });
 
 app.listen(PORT, () => {
   console.log("Hi, its server");
