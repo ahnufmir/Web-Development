16aa66aab32e6b44fe201f5f95c6802066f462ae
Ahnuf Mir <mirahnaf55@gmail.com>
2026-01-23 00:18:40 +0500

    added get method for browser

--- Backend Development/restAPI.js
+++ Backend Development/restAPI.js
@@ -4,15 +4,29 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
+// for getting specfic user from any client other than browers
 app.get("/api/users/:id", (req,res)=>{
-    const id = req.params.id;
+    const id = Number(req.params.id);
     const user = users.find( (user) => id === user.id);
     return res.json(user);
-})
+});
 
+// for listing all users from any client other than browser
 app.get("/api/users", (req, res) => {
     return res.json(users);
 });
+
+//for lishting all users for browser client
+app.get("/users", (req,res)=>{
+    const html = 
+    `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html);
+
+})
 
 
 
