a90eb8372c36af367cd339c08eb43faf8ae91aed
Ahnuf Mir <mirahnaf55@gmail.com>
2026-01-23 01:10:15 +0500

    added get patch and delete method

--- Backend Development/restAPI.js
+++ Backend Development/restAPI.js
@@ -5,30 +5,40 @@ const app = express();
 const PORT = 8000;
 
 // for getting specfic user from any client other than browers
-app.get("/api/users/:id", (req,res)=>{
-    const id = Number(req.params.id);
-    const user = users.find( (user) => id === user.id);
-    return res.json(user);
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => id === user.id);
+  return res.json(user);
 });
 
 // for listing all users from any client other than browser
 app.get("/api/users", (req, res) => {
-    return res.json(users);
+  return res.json(users);
 });
 
 //for lishting all users for browser client
-app.get("/users", (req,res)=>{
-    const html = 
-    `
+app.get("/users", (req, res) => {
+  const html = `
     <ul>
     ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
     </ul>
-    `
-    res.send(html);
+    `;
+  res.send(html);
+});
 
-})
-
-
+app 
+    .route("/api/users/:id")
+    .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => id === user.id);
+    return res.json(user);
+    })
+    .patch((req,res)=>{
+        return res.json({status:"pending"});
+    })
+    .delete((req,res)=>{
+        return res.json({status:"pending"});
+    })
 
 app.listen(PORT, () => {
   console.log("Hi, its server");
