b424bedcb0df8e84f9fe4d0421d96d32fe057232
Ahnuf Mir <mirahnaf55@gmail.com>
2026-01-27 01:37:52 +0500

    added record in mongoDB through restAPI

--- Backend Development/restAPI.js
+++ Backend Development/restAPI.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
 const fs = require("fs");
 const { builtinModules } = require("module");
 
@@ -7,6 +8,36 @@ const app = express();
 const PORT = 8001;
 
 app.use(express.urlencoded({ extended: false }));
+
+const schema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    require: true,
+  },
+  last_name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+    require: true,
+  },
+  ip_address: {
+    type: String,
+    unique: true,
+  },
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/My-Practise-DB")
+  .then(console.log("Moongose Connected"))
+  .catch((err) => console.log("Mongo Error", err));
+  
+const User = new mongoose.model("tests", schema);
 
 app.use((req, res, next) => {
   console.log("check kr meri bandmashi im middle ware 1 hahah!!");
@@ -51,31 +82,49 @@ app
     const user = users.find((user) => id === user.id);
     return res.json(user);
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
     const body = req.body;
-    users.push({ ...body, id: users.length + 1 });
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.send(body);
-    });
+    if(!body.first_name || !body.email || !body.gender || !body.ip_address)
+        return res.status(400).json({msg : "All fields are required"})
+    const result = await User.create({
+        first_name : body.first_name,
+        last_name : body.last_name,
+        gender : body.gender,
+        email : body.email,
+        ip_address : body.ip_address
+    })
+    console.log("Result " , result);
+    return res.status(201).json({msg : "Entity Createad"});
+    // users.push({ ...body, id: users.length + 1 });
+    // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    //   return res.send(body);
+    // });
   })
   .patch((req, res) => {
     const body = req.body;
     console.log(body);
-    const modifiedUser = users.find((user) => user.id === Number(req.params.id));
-    const allUsersExceptModified = users.filter((user)=> user.id !== Number(req.params.id));
+    const modifiedUser = users.find(
+      (user) => user.id === Number(req.params.id),
+    );
+    const allUsersExceptModified = users.filter(
+      (user) => user.id !== Number(req.params.id),
+    );
     modifiedUser.id = Number(req.params.id);
     modifiedUser.first_name = body.first_name;
     modifiedUser.last_name = body.last_name;
     modifiedUser.email = body.email;
     modifiedUser.gender = body.gender;
     modifiedUser.ip_address = body.ip_address;
-    allUsersExceptModified.splice(Number(req.params.id), 0, modifiedUser );
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(allUsersExceptModified), (err, data) => {
-      return res.send(body);
-    });
+    allUsersExceptModified.splice(Number(req.params.id), 0, modifiedUser);
+    fs.writeFile(
+      "./MOCK_DATA.json",
+      JSON.stringify(allUsersExceptModified),
+      (err, data) => {
+        return res.send(body);
+      },
+    );
   })
   .delete((req, res) => {
-    
     // const toDeleteUser = users.find((user) => user.id === body.id);
     // const index = users.indexOf(toDeleteUser);
     // if(index >= 0)
@@ -84,12 +133,18 @@ app
     //     return res.send("User not Found");
     //console.log("Body: ", req.params.id);
     //console.log("Users: ", users);
-    const updatedUsers = users.filter((user)=> user.id !== Number(req.params.id));
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(updatedUsers), (err,data)=>{
+    const updatedUsers = users.filter(
+      (user) => user.id !== Number(req.params.id),
+    );
+    fs.writeFile(
+      "./MOCK_DATA.json",
+      JSON.stringify(updatedUsers),
+      (err, data) => {
         return res.json(updatedUsers);
-    })
+      },
+    );
   });
 
 app.listen(PORT, () => {
-  console.log("Hi, its server");
+  console.log("Connected on port ", PORT);
 });
